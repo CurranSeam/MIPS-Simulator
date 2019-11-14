472c83f3ef6508b1fc05087e74a1c20d5ea7d686
Curran Seam <49770988+CurranSeam@users.noreply.github.com>
2019-11-14 12:59:49 -0800

    Added beq, bne, j, and jr methods

--- src/structures/Computer.java
+++ src/structures/Computer.java
@@ -49,17 +49,17 @@ public class Computer {
 	}
 
 	// TODO - Set CC (remove this after implementing)
-	/**
-	 * Performs not operation by using the data from the register based on bits[7:9] 
-	 * and inverting and storing in the register based on bits[4:6]
-	 */
-	public void executeNot() {
-		BitString destBS = mIR.substring(4, 3);
-		BitString sourceBS = mIR.substring(7, 3);
-		mRegisters[destBS.getValue()] = mRegisters[sourceBS.getValue()].copy();
-		mRegisters[destBS.getValue()].invert();
-		setCC(destBS);
-	}
+//	/**
+//	 * Performs not operation by using the data from the register based on bits[7:9] 
+//	 * and inverting and storing in the register based on bits[4:6]
+//	 */
+//	public void executeNot() {
+//		BitString destBS = mIR.substring(4, 3);
+//		BitString sourceBS = mIR.substring(7, 3);
+//		mRegisters[destBS.getValue()] = mRegisters[sourceBS.getValue()].copy();
+//		mRegisters[destBS.getValue()].invert();
+//		setCC(destBS);
+//	}
 	
 	public void executeAdd() {
 		BitString destBS = mIR.substring(16, 5);
@@ -137,8 +137,8 @@ public class Computer {
 		immNum.signExtend();
 		StringBuilder result = new StringBuilder();
 		for(int i = 0; i < source1BS.getLength()  - 1; i++) {
-			if ((source1BS.toString().charAt(i) == '1' 
-					&& immNum.toString().charAt(i) == '1')) {
+			if (mRegisters[source1BS.getValue()].toString().charAt(i) == '1' 
+					&& immNum.toString().charAt(i) == '1') {
 				result.append("1");
 			} else {
 				result.append("0");	
@@ -157,8 +157,8 @@ public class Computer {
 		BitString source2BS = mIR.substring(11, 5);
 		StringBuilder result = new StringBuilder();
 		for(int i = 0; i < source1BS.getLength()  - 1; i++) {
-			if ((source1BS.toString().charAt(i) == '1' 
-					|| source2BS.toString().charAt(i) == '1')) {
+			if (mRegisters[source1BS.getValue()].toString().charAt(i) == '1' 
+					|| mRegisters[source2BS.getValue()].toString().charAt(i) == '1') {
 				result.append("1");
 			} else {
 				result.append("0");	
@@ -172,14 +172,14 @@ public class Computer {
 	}
 	
 	public void executeOri() {
-		BitString destBS = mIR.substring(16, 5);
+		BitString destBS = mIR.substring(11, 5);
 		BitString source1BS = mIR.substring(6, 5);
 		BitString immNum = mIR.substring(16, 16);
 		immNum.signExtend();
 		StringBuilder result = new StringBuilder();
 		for(int i = 0; i < source1BS.getLength()  - 1; i++) {
-			if ((source1BS.toString().charAt(i) == '1' 
-					|| immNum.toString().charAt(i) == '1')) {
+			if (mRegisters[source1BS.getValue()].toString().charAt(i) == '1' 
+					|| immNum.toString().charAt(i) == '1') {
 				result.append("1");
 			} else {
 				result.append("0");	
@@ -198,7 +198,7 @@ public class Computer {
 		int location = mPC.getValue() + pcOffset.getValue2sComp();
 		BitString valueAtLocation = mMemory[location];
 		mRegisters[destBS.getValue()].setValue2sComp(valueAtLocation.getValue2sComp());
-		setCC(destBS);
+//		setCC(destBS);
 	}
 	
 	/**
@@ -213,46 +213,70 @@ public class Computer {
 		mMemory[address] = word;
 	}
 	
-	public void executeBranch() {
-		BitString pcOffset = mIR.substring(7, 9);
-		int negative = mIR.substring(4, 1).getValue();
-		int zero = mIR.substring(5, 1).getValue();
-		int positive = mIR.substring(6, 1).getValue();
-		int cc0 = mCC.substring(0, 1).getValue();
-		int cc1 = mCC.substring(1, 1).getValue();
-		int cc2 = mCC.substring(2, 1).getValue();
-		if (negative + cc0 == 2 || zero + cc1 == 2 || positive + cc2 == 2 ) {
-			mPC.setValue2sComp(mPC.getValue() + pcOffset.getValue2sComp());
-		}
-	}
+//	public void executeBranch() {
+//		BitString pcOffset = mIR.substring(7, 9);
+//		int negative = mIR.substring(4, 1).getValue();
+//		int zero = mIR.substring(5, 1).getValue();
+//		int positive = mIR.substring(6, 1).getValue();
+//		int cc0 = mCC.substring(0, 1).getValue();
+//		int cc1 = mCC.substring(1, 1).getValue();
+//		int cc2 = mCC.substring(2, 1).getValue();
+//		if (negative + cc0 == 2 || zero + cc1 == 2 || positive + cc2 == 2 ) {
+//			mPC.setValue2sComp(mPC.getValue() + pcOffset.getValue2sComp());
+//		}
+//	}
 	
 	public void executeBeq() {
+		BitString source1BS = mIR.substring(6, 5);
+		BitString source2BS = mIR.substring(11, 5);
+		BitString labelBS = mIR.substring(16, 16);
+		int offset = (labelBS.getValue2sComp() - mPC.getValue()) / 4
 		
+		BitString source1 = mRegisters[source1BS.getValue()];
+		BitString source2 = mRegisters[source2BS.getValue()];
+		
+		if (source1.getValue() == source2.getValue())) {
+			mPC.setValue2sComp(mPC.getValue() + offset);
+		}
 	}
 	
 	public void executeBne() {
+		BitString source1BS = mIR.substring(6, 5);
+		BitString source2BS = mIR.substring(11, 5);
+		BitString labelBS = mIR.substring(16, 16);
+		int offset = (labelBS.getValue2sComp() - mPC.getValue()) / 4
 		
+		BitString source1 = mRegisters[source1BS.getValue()];
+		BitString source2 = mRegisters[source2BS.getValue()];
+		
+		if (source1.getValue() != source2.getValue())) {
+			mPC.setValue(mPC.getValue() + offset);
+		}
 	}
 	
 	public void executeJ() {
-		
+		BitString addressBS = mIR.substring(6, 26);
+		mPC.setValue(addressBS.getValue());
 	}
 	
 	public void executeJr() {
+		BitString registerBS = mIR.substring(6, 5);
+		BitString addressBS = mRegisters[registerBS.getValue()];
 		
+		mPC.setValue(addressBS.getValue());
 	}
 	
-	public void setCC(BitString destBS) {
-		BitString bitsInRegister = mRegisters[destBS.getValue()];
-		int valueInRegister = bitsInRegister.getValue();
-		if (valueInRegister > 0) {
-			mCC.setBits(new char[] {'0', '0', '1'});
-		} else if (valueInRegister == 0) {
-			mCC.setBits(new char[] {'0', '1', '0'});
-		} else if (valueInRegister < 0) {
-			mCC.setBits(new char[] {'1', '0', '0'});
-		}
-	}
+//	public void setCC(BitString destBS) {
+//		BitString bitsInRegister = mRegisters[destBS.getValue()];
+//		int valueInRegister = bitsInRegister.getValue();
+//		if (valueInRegister > 0) {
+//			mCC.setBits(new char[] {'0', '0', '1'});
+//		} else if (valueInRegister == 0) {
+//			mCC.setBits(new char[] {'0', '1', '0'});
+//		} else if (valueInRegister < 0) {
+//			mCC.setBits(new char[] {'1', '0', '0'});
+//		}
+//	}
 	
 	public void executeOut() {
 		int asciiCode = mRegisters[0].getValue();
@@ -308,9 +332,9 @@ public class Computer {
 		mPC.display(true);
 		System.out.print("   ");
 
-		System.out.print("CC ");
-		mCC.display(true);
-		System.out.println("   ");
+//		System.out.print("CC ");
+//		mCC.display(true);
+//		System.out.println("   ");
 
 		for (int i = 0; i < MAX_REGISTERS; i++) {
 			System.out.printf("R%d ", i);
